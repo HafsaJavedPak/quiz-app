c641e42675bcff42d4fef3e9b235a230337e4051
Hafsa Javed <hafsajavedpak@outlook.com>
2024-02-20 09:20:48 +0500

    removed "/" from static_root in settings.py

--- django_school/settings.py
+++ django_school/settings.py
@@ -123,7 +123,7 @@ if DEBUG :
         ('', os.path.join(BASE_DIR, 'static')),
     ]
 else :
-    STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'classroom.User'
 
